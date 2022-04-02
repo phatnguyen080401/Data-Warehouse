3b943c929fed7b6b31bebd34104982dc9d4f0d1d
Hyphersism <nguyenphat0804@gmail.com>
2022-04-02 17:03:54 +0700

    Add models DAGs

---
+++ dbt-airflow/dags/models.py
@@ -0,0 +1,69 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+import os
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020,8,1),
+    'retries': 0
+}
+
+with DAG('transformation_analysis', default_args=default_args, schedule_interval='@once') as dag:
+    staging = BashOperator(
+        task_id='staging',
+        bash_command='cd /dbt && dbt run --models staging --profiles-dir .',
+        env={
+            'dbt_user': '{{ var.value.dbt_user }}',
+            'dbt_password': '{{ var.value.dbt_password }}',
+            **os.environ
+        },
+        dag=dag
+    )
+
+    score = BashOperator(
+        task_id='IMDB_weighted_rating',
+        bash_command='cd /dbt && dbt run --models marts.score --profiles-dir .',
+        env={
+            'dbt_user': '{{ var.value.dbt_user }}',
+            'dbt_password': '{{ var.value.dbt_password }}',
+            **os.environ
+        },
+        dag=dag
+    )
+
+    q_movies = BashOperator(
+        task_id='q_movies',
+        bash_command='cd /dbt && dbt run --models marts.q_movies --profiles-dir .',
+        env={
+            'dbt_user': '{{ var.value.dbt_user }}',
+            'dbt_password': '{{ var.value.dbt_password }}',
+            **os.environ
+        },
+        dag=dag
+    )
+
+    movies = BashOperator(
+        task_id='tmdb_movies',
+        bash_command='cd /dbt && dbt run --models marts.movies --profiles-dir .',
+        env={
+            'dbt_user': '{{ var.value.dbt_user }}',
+            'dbt_password': '{{ var.value.dbt_password }}',
+            **os.environ
+        },
+        dag=dag
+    )
+
+    credits = BashOperator(
+        task_id='tmdb_credits',
+        bash_command='cd /dbt && dbt run --models marts.credits --profiles-dir .',
+        env={
+            'dbt_user': '{{ var.value.dbt_user }}',
+            'dbt_password': '{{ var.value.dbt_password }}',
+            **os.environ
+        },
+        dag=dag
+    )
+
+    staging >> score >> q_movies >> [credits, movies] 
--- dbt-airflow/dags/seed_data.py
+++ dbt-airflow/dags/seed_data.py
@@ -1,22 +1,18 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
-from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
 import os
-
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2020,8,1),
     'retries': 0
-}
+}    
 
-
-with DAG('1_init_once_seed_data', default_args=default_args, schedule_interval='@once') as dag:
-    task_1 = BashOperator(
-        task_id='load_seed_data_once',
+with DAG('dbt_seed_data', default_args=default_args, schedule_interval='@once') as dag:
+    seed_data = BashOperator(
+        task_id='load_seed_data',
         bash_command='cd /dbt && dbt seed --profiles-dir .',
         env={
             'dbt_user': '{{ var.value.dbt_user }}',
@@ -26,4 +22,4 @@ with DAG('1_init_once_seed_data', default_args=default_args, schedule_interval='
         dag=dag
     )
 
-task_1 
+seed_data 
